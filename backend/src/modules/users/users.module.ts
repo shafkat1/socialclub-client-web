ee380a8ca9a5de06cf36198cb41050f3db3d00bd
shafkat mustafa <shafkat@hotmail.com>
2025-10-31 20:44:35 -0400

    fix: Add S3Service to UsersModule providers - resolves NestJS DI error

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { PrismaService } from '../../common/services/prisma.service';
+import { S3Service } from '../../common/services/s3.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, S3Service],
   exports: [UsersService],
 })
 export class UsersModule {}
